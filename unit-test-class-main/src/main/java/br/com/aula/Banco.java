1e503ca8fa9f1d9509b1221997be5cd53372909e
Leo <leoquezado@outlook.com>
2021-10-24 23:48:08 -0300

    Test

---
+++ unit-test-class-main/src/main/java/br/com/aula/Banco.java
@@ -0,0 +1,100 @@
+package br.com.aula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.aula.exception.ContaComNomeJaExistente;
+import br.com.aula.exception.ContaComNumueroInvalido;
+import br.com.aula.exception.ContaJaExistenteException;
+import br.com.aula.exception.ContaNaoExistenteException;
+import br.com.aula.exception.ContaSemSaldoException;
+import br.com.aula.exception.TransferenciaNegativa;
+
+public class Banco {
+
+	private List<Conta> contas = new ArrayList<Conta>();
+
+	public Banco() {
+	}
+
+	public Banco(List<Conta> contas) {
+		this.contas = contas;
+	}
+
+
+	public void cadastrarConta(Conta conta) throws ContaJaExistenteException, ContaComNomeJaExistente, ContaComNumueroInvalido {
+
+		for (Conta c : contas) {
+			boolean isNomeClienteIgual = c.getCliente().getNome().equals(conta.getCliente().getNome());
+			boolean isNumeroContaIgual = c.getNumeroConta() == conta.getNumeroConta();
+			boolean isNumerContaInvalido = c.getNumeroConta() > 9999999;
+
+			if (isNomeClienteIgual ) {
+				throw new ContaJaExistenteException();
+			}
+			
+			if(isNumeroContaIgual) {
+				throw new ContaComNomeJaExistente();
+
+			}
+			
+			if(isNumerContaInvalido) {
+				throw new ContaComNumueroInvalido();
+
+			}
+			
+			
+		}
+		
+		this.contas.add(conta);
+
+	}
+
+	public void efetuarTransferencia(int numeroContaOrigem, int numeroContaDestino, int valor)
+			throws ContaNaoExistenteException, ContaSemSaldoException, TransferenciaNegativa {
+
+		Conta contaOrigem = this.obterContaPorNumero(numeroContaOrigem);
+		Conta contaDestino = this.obterContaPorNumero(numeroContaDestino);
+
+		boolean isContaOrigemExistente = contaOrigem != null;
+		boolean isContaDestinoExistente = contaDestino != null;
+
+		if (isContaOrigemExistente && isContaDestinoExistente) {
+
+			boolean isContaOrigemPoupança = contaOrigem.getTipoConta().equals(TipoConta.POUPANCA);
+			boolean isSaldoContaOrigemNegativo = contaOrigem.getSaldo() - valor < 0;
+			boolean isSaldoContaDestinoNegativo = contaOrigem.getSaldo() - valor < 0;
+			
+			
+			if (isContaOrigemPoupança && isSaldoContaDestinoNegativo) {
+				throw new TransferenciaNegativa();
+			}
+
+
+			if (isContaOrigemPoupança && isSaldoContaOrigemNegativo) {
+				throw new ContaSemSaldoException();
+			}
+
+			contaOrigem.debitar(valor);
+			contaDestino.creditar(valor);
+
+		} else {
+			throw new ContaNaoExistenteException();
+		}
+	}
+
+	public Conta obterContaPorNumero(int numeroConta) {
+
+		for (Conta c : contas) {
+			if (c.getNumeroConta() == numeroConta) {
+				return c;
+			}
+		}
+
+		return null;
+	}
+
+	public List<Conta> obterContas() {
+		return this.contas;
+	}
+}
